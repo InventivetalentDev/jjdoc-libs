aa1ae966b12675d8687dc129c68df760a9ae2ec9
inventivetalent <mail@inventivetalent.org>
2019-02-02 16:51:32 +0100

    add VaultAPI

---
+++ libs/VaultAPI/generate.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+LIB_NAME="VaultAPI"
+SOURCE_PATH="src/main/java"
+PACKAGES="net.milkbowl.vault"
+
+# cd out to root dir
+cd ../..
+
+# cd into scripts
+cd scripts/
+
+# run generate with args
+chmod +x generate.sh
+./generate.sh ${LIB_NAME} ${SOURCE_PATH} ${PACKAGES}
+
+# cd back into lib dir
+cd ..
+cd libs/${LIB_NAME}
+
+# commit
+git add .
+git commit -m "[skip ci] Update $LIB_NAME"
