481ffaf2c97a706fe3a6bb8f09cad0f42cb75552
inventivetalent <mail@inventivetalent.org>
2019-02-02 10:58:12 +0100

    add packages arg

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,6 +6,7 @@
 DOCLET_VERSION="1.0.3"
 LIB_NAME=$1
 SOURCE_PATH=$2
+PACKAGES=$3
 
 # Create temp directory & cd into it
 mkdir temp
@@ -20,7 +21,7 @@ cd ..
 # Run Doclet
 # - Takes sources from /libs/.../source/... - which should be a submodule of the library's source code
 # - Outputs to /libs/.../jjdoc & /libs/.../jjdoc-....json
-javadoc -docletpath ./temp/json-doclet-$DOCLET_VERSION-SNAPSHOT-jar-with-dependencies.jar -doclet org.inventivetalent.jsondoclet.JsonDoclet -indexfile -outdir ../libs/$LIB_NAME/jjdoc  -singlefile -outfile ../libs/$LIB_NAME/jjdoc-$LIB_NAME.json -sourcepath ../libs/$LIB_NAME/source/$SOURCE_PATH -public
+javadoc -docletpath ./temp/json-doclet-$DOCLET_VERSION-SNAPSHOT-jar-with-dependencies.jar -doclet org.inventivetalent.jsondoclet.JsonDoclet -indexfile -outdir ../libs/$LIB_NAME/jjdoc  -singlefile -outfile ../libs/$LIB_NAME/jjdoc-$LIB_NAME.json -sourcepath ../libs/$LIB_NAME/source/$SOURCE_PATH -public -subpackages $PACKAGES
 
 ## Final cleanup
 rm -Rf temp
